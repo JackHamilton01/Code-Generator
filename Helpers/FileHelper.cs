149aa0ace185ba6c7796e375560a02cd608cd4ac
JackHamilton01 <jackhamilton2807@gmail.com>
2022-03-18 18:23:37 +0000

    Functionality added, images and chords display in random order when start button is pressed, training beat added to play in background

---
+++ Helpers/FileHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chord_Generator.Helpers
+{
+    public static class FileHelper
+    {
+
+        public static string GetPath(string fileName, string folder)
+        {
+            return Path.Combine(Directory.GetCurrentDirectory() + folder, fileName);
+        }
+    }
+}
---
+++ Services/AudioService.cs
@@ -0,0 +1,28 @@
+﻿using Chord_Generator.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chord_Generator.Services
+{
+    public class AudioService
+    {
+        private string audioFolder = @"\Audio\";
+        private string trainingBeatFileName = "Trainingbeat.WAV";
+
+        SoundPlayer soundPlayer;
+
+        public AudioService()
+        {
+            soundPlayer = new SoundPlayer(FileHelper.GetPath(trainingBeatFileName, audioFolder));
+        }
+
+        public async Task PlayAudio()
+        {
+            await Task.Run(() => soundPlayer.PlayLooping());
+        }
+    }
+}
